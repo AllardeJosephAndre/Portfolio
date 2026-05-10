0f96cdb63b8ff0507bead723903c47ea0dace339
unknown <andreiallarde@gmail.com>
2026-05-10 16:34:37 +0800

    Portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+
+
+    navLinks.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    this.classList.add('active');
+
+  });
+});
